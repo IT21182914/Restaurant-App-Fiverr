9c19a3cc5016f29a71b9731c82c62b5194997834
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-08-29 16:52:57 +0530

    Init server.js

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(cors());
+app.use(express.json());
+
+// Define Routes
+app.use("/api/menu", require("./routes/menu"));
+app.use("/api/orders", require("./routes/order"));
+app.use("/api/users", require("./routes/user"));
+app.use("/api/qrcode", require("./routes/qrCode"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
