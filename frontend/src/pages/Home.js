b097aec20d92bfe446a17c0a70755356aef7d224
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-08-29 23:24:25 +0530

    Home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Menu from "../components/Menu";
+import OrderForm from "../components/OrderForm";
+import QRCodeGenerator from "../components/QRCodeGenerator";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Menu />
+      <div className="container mx-auto py-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <OrderForm />
+          <QRCodeGenerator />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
