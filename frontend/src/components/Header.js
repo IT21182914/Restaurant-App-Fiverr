2a0aaf4e4673b547e20f64bb570cabb6bf648a82
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-08-29 23:31:46 +0530

    Simple Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white p-6 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-3xl font-bold">Restaurant Order Management</h1>
+        <nav>
+          <Link className="mr-6 hover:text-gray-300" to="/">
+            Home
+          </Link>
+          <Link className="hover:text-gray-300" to="/orders">
+            Orders
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
