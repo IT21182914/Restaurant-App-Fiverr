6bd785985e1bfe452c8ce0d3c161097cb55c94cb
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-08-29 16:55:41 +0530

    Updates

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB Connected...");
+    await mongoose.connect(process.env.MONGO_URI, {});
+    console.log("MongoDB Connected Successfully 🍃");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
     process.exit(1);
--- backend/server.js
+++ backend/server.js
@@ -19,4 +19,4 @@ app.use("/api/qrcode", require("./routes/qrCode"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`\nServer running on port ${PORT} 🔥`));
