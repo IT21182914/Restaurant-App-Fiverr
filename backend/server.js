0e1f32f96958afd4c88c9930562bd25300363432
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-08-29 23:04:22 +0530

    Code Quality

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
-require("dotenv").config();
 const express = require("express");
-const connectDB = require("./config/db");
 const cors = require("cors");
+
+const connectDB = require("./config/db");
+
+require("dotenv").config();
 
 const app = express();
 
